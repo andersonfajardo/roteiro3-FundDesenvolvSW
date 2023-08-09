4a2dcd3a46f76437dbd305b1a575317fcffae20b
Anderson Fajardo <110858277+andersonfajardo@users.noreply.github.com>
2023-08-09 18:25:12 -0300

    Update App.java terceiro commit

--- App.java
+++ App.java
@@ -1,3 +1,5 @@
 //adicionei um comentário para poder fazer um commit
 
+//estou fazendo a minha segunda alteração
+
 public class NumFinder { private int smallest = Integer.MAX_VALUE; private int largest = Integer.MIN_VALUE; public void find(int[] nums) { for(int n : nums) { if(n < smallest)smallest = n; else if (n > largest) largest = n; } } public int getSmallest () { return smallest; } public int getLargest () { return largest; } }
